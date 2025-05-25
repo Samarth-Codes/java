21335c7d7f06ae76a6920f10807e64d1c8afa235
Samarth Saxena <154627397+Samarth-Codes@users.noreply.github.com>
2025-05-25 13:13:53 +0530

    Create arithmeticoperator.java

---
+++ arithmeticoperator.java
@@ -0,0 +1,27 @@
+import java.lang.*;
+
+
+public class arithmeticoperator {
+    public static void main(String[] args) {
+
+        int a= 10;
+        int b=3;
+
+        // addition
+        System.out.println(a+b);
+
+        //subtraction
+        System.out.println(a-b);
+
+        //multiplication
+        System.out.println(a*b);
+
+        //division
+        System.out.println(a/b);
+
+
+        //Modulo
+        System.out.println(a%b);
+
+    }
+}
