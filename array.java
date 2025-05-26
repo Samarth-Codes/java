038e24b23db16674fe8a29f816e897dfe7c8d43e
Samarth Saxena <154627397+Samarth-Codes@users.noreply.github.com>
2025-05-26 13:06:21 +0530

    Create array.java

---
+++ array.java
@@ -0,0 +1,27 @@
+public class array {
+    public static void main(String[] args){
+        int[] marks =new int[10];
+        marks[1]=5;
+        marks[2]=8;
+
+        System.out.println(marks[1]);
+        System.out.println(marks[0]);
+        System.out.println(marks[2]);
+
+
+        System.out.println(marks);
+
+        float[] arr= new float[5];
+        System.out.println(arr[1]);
+
+        String[] names={"ccc","dddd","aaa"};
+
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+
+
+
+
+
+    }
+}
