bb451c918dea21951ac26594c5397f1f9f424cfb
Samarth Saxena <154627397+Samarth-Codes@users.noreply.github.com>
2025-05-31 23:00:42 +0530

    Create breakstatement.java

---
+++ breakstatement.java
@@ -0,0 +1,14 @@
+public class breakstatement {
+public static void main (String[] args){
+    int i=1;
+    while(i<=10){
+
+        if(i==7){
+            System.out.println("Thala for a reason");
+            break;}
+        System.out.println(i);
+        i++;
+        }
+    }
+}
+
